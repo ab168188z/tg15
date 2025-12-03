15305521165cfa73123fa2c5fd46df336b45b61d
ab168188z <ttle30674@gmail.com>
2025-12-03 13:12:16 +0800

    Add files via upload

---
+++ static/js/download.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    const downloadBtns = document.querySelectorAll('.td_download_btn');
+    downloadBtns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault(); // 阻止默认跳转行为
+            const fileUrl = btn.dataset.url;
+            
+            const url = "/api/download?url=" + encodeURIComponent(fileUrl);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "telegram.zip"; // 可根据 fileUrl 动态设置
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+            
+        });
+    });
+};
